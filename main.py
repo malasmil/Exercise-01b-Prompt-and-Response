2bb4a3273745c0d23b14582a68147b57fe75399e
malasmil <89554181+malasmil@users.noreply.github.com>
2021-08-31 10:47:28 -0400

    Update main.py
    intro code to zork interactive fiction game.

--- main.py
+++ main.py
@@ -2,5 +2,11 @@
 import sys
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
 
-passage_name = "West of House"
-passage_text = "This is an open field west of a white house, with a boarded front door."
+passage_name = "Starting Room"
+passage_text = "You wake up to find yourself to what seems to be a cave. There is a single lit torch laying on the ground. There are entry ways facing North, South, East and West."
+
+print("you are at the " + passage_name)
+print(passage_text)
+response = input("What would you like to do? ")
+if response == "go north":
+    print("good job!")
